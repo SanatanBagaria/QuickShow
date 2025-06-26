68f34d1cdf677147dcfcdda84e803889d7daf750
SanatanBagaria <bagariasanatan@gmail.com>
2025-06-26 22:17:41 +0530

    changing imports still

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import AdminNavbar from '../../components/admin/adminNavbar'
+import AdminNavbar from '../../components/admin/AdminNavbar'
 import AdminSidebar from '../../components/admin/AdminSidebar'
 import { Outlet } from 'react-router-dom'
 import { useAppContext } from '../../context/AppContext'
